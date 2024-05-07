1491459bd313ddad0a52eefc73303401261346bd
gtawijayanti <169070712+gtawijayanti@users.noreply.github.com>
2024-05-07 20:12:16 +0700

    Update home_0.php

--- includes/home_0.php
+++ includes/home_0.php
@@ -1,46 +1,31 @@
-  <div class="row-fluid">
-	<div class="span3 well">
-		<form method="post" action="checklogin.php">
-			<label>
-				<input name="username" id="username" type="text" value="" placeholder="Username..." class="span12" autofocus>
-			</label>
-			<label>
-				<input name="password" id="password" type="password" value="" placeholder="Password..." class="span12">
-			</label>
-			<label>
-				<select name="type" class="span12">
-				  <option value=1>Petugas</option>
-				  <option value=2>Dosen</option>
-				  <option value=3>Mahasiswa</option>
-				</select>
-			</label>
-			<label>
-				<input name="login" id="login" type="submit" value="Login" class="btn btn-block btn-inverse">
-			</label>
-		</form>
+  <div class="row-fluid nav-left">
+	<div class="span3 well scroll">
+	  <div class="sidebar-nav">
+		<ul class="nav nav-list">
+		  <li class="nav-header">Kategori</li>
+		  <?php	
+			$db=new MySQL();
+			$db->connect();
+			$db->execute("SELECT kodeKategori, namaKategori
+				FROM tb_kategori"); 
+			$cat=$db->get_dataset();
+			for($i=0; $i<count($cat); $i++) {
+		  ?>
+			<li><a href="?cat=<?=$cat[$i][0]?>"><?=$cat[$i][1]?></a></li>
+		  <?php } ?>
+		</ul>
+	  </div><!--/.well -->
 	</div><!--/span-->
+	
 	<div class="span9 well" id="main-content">
 	  <div class="row-fluid">
 		<div class="span12">
-		
 		  <?php 
-			if(!isset($page)) {
+			if(!isset($page))
+				include("includes/m_home.php"); 
+			else
+				include("includes/m_". $page .".php"); 
 		  ?>
-			  <h2>Selamat Datang</h2>
-			  <p>Anda dapat melakukan login di panel sebelah kiri, kemudian melakukan peminjaman buku.</p>
-			  <p>Batas peminjaman buku adalah maks 1 x 2 buku / hari</p>
-			  
-			  <!--
-			  <a href="?page=register" rel="tooltip" title="Daftar" class="btn btn-large btn-success">
-				<i class="icon-user icon-white"></i> Daftar
-			  </a>
-			  -->
-			  <br /><br /><br /><br /><br />
-		  <?php
-			} else
-				include("includes/p_". $page .".php"); 
-		  ?>
-		  
 		</div><!--/span-->
 	  </div><!--/row-->
 	</div><!--/span-->
